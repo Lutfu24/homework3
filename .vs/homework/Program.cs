303d4b47740d1c049ab121983b73b047aa169af2
Lutfu24 <lutfuhm@code.edu.az>
2022-11-22 16:33:34 +0400

    homework

---
+++ .vs/homework/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Num();
+        }
+        static void Num ()
+        {
+            Console.WriteLine("Please enter a number");
+            string numStr = Console.ReadLine();
+            int number = Convert.ToInt32(numStr);
+            if (number % 7 == 0)
+            {
+                Console.WriteLine("7-ye bolunur");
+            }
+            else if (number%7>=4)
+            {
+                Console.WriteLine(number+7-number%7);
+            }
+            else
+            {
+                Console.WriteLine(number-number%7);
+            }
+        }
+    }
+}
---
+++ homework/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+
+namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Num();
+        }
+        static void Num()
+        {
+            Console.WriteLine("Please enter a number");
+            int number = int.Parse(Console.ReadLine());
+            if (number%7==0)
+            {
+                Console.WriteLine("7-ye bolunur");
+            }
+            else if (number%7>=4)
+            {
+                Console.WriteLine(number+7-number%7);
+            }
+            else
+            {
+                Console.WriteLine(number-number%7);
+            }
+        }
+    }
+}
